35167c3036eae9888c36eb24bd168c24b0732d65
Ruslan <Ruslan@HP-PAVILION>
2021-12-09 19:15:57 +0200

    qwe

--- app.js
+++ app.js
@@ -59,5 +59,3 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 module.exports = app;
-
-//danil
--- routes/register.js
+++ routes/register.js
@@ -1,28 +1,32 @@
 var express = require("express");
 var router = express.Router();
 const db = require("../database.js");
+const bcrypt = require("bcrypt");
 
 router.use(express.json());
-router.use(express.urlencoded({extended: false})); 
-const urlencodedParser = express.urlencoded({extended: false});
+router.use(express.urlencoded({ extended: false }));
+const urlencodedParser = express.urlencoded({ extended: false });
 
 router.get("/", function (req, res, next) {
   res.render("register", { title: "Register | Newspaper" });
 });
 
 router.post("/", urlencodedParser, function (req, res) {
-  var data = [req.body.name, req.body.email, req.body.password];
-  var sql = "INSERT INTO user (name, email, password, failed_logins) VALUES (?,?,?,0)";
+  bcrypt.hash(req.body.password, 10, function (err, hash) {
+    var data = [req.body.name, req.body.email, hash];
+    var sql =
+      "INSERT INTO user (name, email, password, failed_logins) VALUES (?,?,?,0)";
     db.run(sql, data, function (err, result) {
-    if (err) {
-      res.status(400);
-      res.send("database error:" + err.message);
-      return;
-    }
-    res.render("index", {
-      activePage: "register",
-      formData: req.body,
-      title: "Login | Newspaper"
+      if (err) {
+        res.status(400);
+        res.send("database error:" + err.message);
+        return;
+      }
+      res.render("index", {
+        activePage: "register",
+        formData: req.body,
+        title: "Login | Newspaper",
+      });
     });
   });
 });
