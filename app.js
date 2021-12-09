e44e3379c1409404b937b1de58ce6fdfee6597c7
Ruslan <Ruslan@HP-PAVILION>
2021-12-09 21:05:12 +0200

    pudge

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var loginRouter = require("./routes/login");
 var registerRouter = require("./routes/register");
+var addArticleRouter = require("./routes/add_article");
 
 var app = express();
 
@@ -38,6 +39,7 @@ app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/login", loginRouter);
 app.use("/register", registerRouter);
+app.use("/add_article", addArticleRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
